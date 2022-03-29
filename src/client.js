19404777f60a81e92ca4c1ea696b657b5f2a30bf
Michael Tolchinsky <mishatolchinski@gmail.com>
2022-03-29 11:33:44 +0300

    fixed comments bug

--- src/client.js
+++ src/client.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from "@sanity/image-url";
 export const client = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
-  apiVersion: "2022-03-24",
+  apiVersion: "2021-10-21",
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
